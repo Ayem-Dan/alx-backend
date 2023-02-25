e20753ca2bd225e741caf6a07812bcb88e483133
Ayem-Dan <danielMMomodu@gmail.com>
2023-02-25 18:04:23 +0100

    Update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,12 +1,20 @@
+#!/usr/bin/python3
+"""Module for FIFOCache class that inherits from BaseCaching and is a \
+      caching system"""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """FIFOCache class that inherits from BaseCaching"""
+
     def __init__(self):
+        """Constructor method"""
         super().__init__()
         self.key_list = []
 
     def put(self, key, item):
+        """Assign to the dictionary self.cache_data the item value for \
+        the key"""
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first_key = self.key_list.pop(0)
@@ -16,6 +24,7 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
+        """Return the value in self.cache_data linked to key"""
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         else:
