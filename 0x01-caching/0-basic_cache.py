cad26b18db74b58acf357a293ba3a3af59b22792
Ayem-Dan <danielMMomodu@gmail.com>
2023-02-25 18:07:45 +0100

    Update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,12 +1,16 @@
+"""Module of BasicCache class inheriting BaseCaching"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """Methods used in the class"""
     def put(self, key, item):
+        """defines the key and items"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Returns the key and items"""
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         else:
